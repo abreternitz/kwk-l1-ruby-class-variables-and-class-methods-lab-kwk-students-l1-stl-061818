1dda791dd2bd0458a8cfcbd6fa19efbc5e61ae91
abreternitz <aliceburian@gmail.com>
2018-06-22 19:27:31 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,1 +1,16 @@
-
+class Song
+  def initialize(genre,artist,name)
+    @name=name
+    @genre=genre
+    @artist=artist
+  end
+  def genre_of_album
+    @genre
+  end
+  def artist_of_album
+    @artist
+  end
+  def name_of_album
+    @name
+  end
+end
