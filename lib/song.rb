afc5e736d0d0fb1f78fa5ad72198e0a11fc74f6d
abreternitz <aliceburian@gmail.com>
2018-06-22 19:52:48 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,12 +1,16 @@
 class Song
   attr_accessor :genre,:artist,:name
   
+  @@count = 0 
   def initialize(genre,artist,name)
     @name=name
     @genre=genre
     @artist=artist
+    @@count=@@count + 1
   end
-
+  def count
+    @@count
+  end
 end
 
 jon_song = Song.new("Hippity Hoppity","Jon Mendez","Jon's Way")
