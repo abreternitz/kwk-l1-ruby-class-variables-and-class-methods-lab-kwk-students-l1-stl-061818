5e1a664ec575c59919f19f248f1e20ccac7d3c4b
abreternitz <aliceburian@gmail.com>
2018-06-22 19:33:41 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,4 +18,4 @@ end
 jon_song = Song.new("Hippity Hoppity","Jon Mendez","Jon's Way")
 puts jon_song.genre_of_album
 puts jon_song.name_of_album
-puts.jon_song.artist_of_album
+puts jon_song.artist_of_album
