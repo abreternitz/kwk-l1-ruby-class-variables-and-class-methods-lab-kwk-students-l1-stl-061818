e561e902cfbd21c73c954632ca5742dd9acf025b
abreternitz <aliceburian@gmail.com>
2018-06-22 19:37:46 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,24 +1,13 @@
 class Song
+  attr_accessor :genre,:artist,:name
+
   def initialize(genre,artist,name)
     @name=name
     @genre=genre
     @artist=artist
   end
-  def get_all
-    @genre
-    @artist
-    @name
-  end
-  def genre_of_album
-    @genre
-  end
-  def artist_of_album
-    @artist
-  end
-  def name_of_album
-    @name
-  end
+
 end
 
 jon_song = Song.new("Hippity Hoppity","Jon Mendez","Jon's Way")
-puts jon_song.get_all
+puts jon_song.genre
